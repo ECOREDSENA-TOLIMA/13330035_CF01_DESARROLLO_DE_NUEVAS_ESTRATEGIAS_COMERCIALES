5783f181e3add1e8260543b1268e0da4c9ace436
Oscar Espitia <odespitia@sena.edu.co>
2024-09-18 05:01:54 -0500

    ajustes cfa

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,7 +19,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/sgHDiPSsRPc" title="Identificación y análisis del cliente ideal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 </template>
